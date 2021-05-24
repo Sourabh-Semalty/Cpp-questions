e1adb9421859a563a0f8a1b6f6ccb6864a93ffff
Sourabh Semalty <semaltysourabh@gmail.com>
2021-05-24 23:11:49 +0530

    anticlock rotation

---
+++ looping/anti_clock_roration.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+void rotate_by_one(int arr[], int size) {
+  int firstItem = arr[0];
+  for(int i = 0; i<size;i++) {
+      arr[i] = arr[i+1];
+  }
+  arr[size-1] = firstItem;
+}
+
+int main() {
+    int arr[5] = {1,2,3,4,5};
+    int rotation = 2;
+    for(int i = 1;i<= rotation ; i++) {
+      rotate_by_one(arr,5);
+    }
+    for(int i = 0; i < 5;i++) {
+      cout<< arr[i];
+    }
+}
--- looping/tempCodeRunnerFile.cpp
+++ looping/tempCodeRunnerFile.cpp
@@ -1,23 +1,3 @@
-#include<iostream>
-using namespace std;
-
-void printAllAlphabets() {
-  char ch;
-  cout << "A to Z" << endl;
-  for (int i = 65; i <= 90; i++)
-  {
-    ch = i;
-    cout << ch << endl;
-  }
-  cout << "a to z" << endl;
-
-    for (int i = 97; i <= 122; i++)
-  {
-    ch = i;
-    cout << ch << endl;
-  }
-  
-}
-int main() {
-  printAllAlphabets();
-}
+for(int i = 0; i < 5;i++) {
+      cout<< arr[i];
+    }
